c3a43bbb08256003769002b810cc4a4a98572fe8
kuriatsu <kuriatsubayashi712@gmail.com>
2021-12-24 16:02:34 +0900

    finish extractor bag

--- extract_clip.py
+++ extract_clip.py
@@ -5,7 +5,8 @@ import cv2
 import xml.etree.ElementTree as ET
 import numpy as np
 import pickle
-
+import threading
+import random
 def getVideo(filename, image_offset_y, crop_rate):
 
     try:
@@ -47,10 +48,12 @@ def getAtrrib(root, id):
     return None
 
 
-def cutVideo(video, start_frame, end_frame, video_name, res):
+def cutVideo(video, start_frame, end_frame, video_name, crop_value, expand_rate):
     # frame_list = []
+    image_res = [int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(video.get(cv2.CAP_PROP_FRAME_WIDTH))]
+    frame_rate = int(video.get(cv2.CAP_PROP_FPS))
     fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')        # 動画保存時のfourcc設定（mp4用）
-    writer = cv2.VideoWriter(video_name, (res[1], res[0]))
+    writer = cv2.VideoWriter(video_name, fourcc, frame_rate, (image_res[1], image_res[0]))
     for index in range(start_frame, end_frame):
         video.set(cv2.CAP_PROP_POS_FRAMES, index)
         ret, frame = video.read()
@@ -59,15 +62,17 @@ def cutVideo(video, start_frame, end_frame, video_name, res):
             writer.write(frame)
             # frame_list.append(frame)
         else:
+            print("failed to get frame")
             break
 
     writer.release()
 
 
 def getVehicleDirection(vehicle_root, start_frame, end_frame):
+    dist_buf = 0
     for i in range(start_frame, len(vehicle_root)):
-        dist_buf += float(vehicle_root[i].attrib.get('OBD_speed')) * 0.03 / 3.6
-        future_angle = float(vehicle_root[end_frame].attrib.get('yaw')) - float(vehicle_root[i].attrib.get('yaw'))
+        dist_buf += float(vehicle_root[i].get('OBD_speed')) * 0.03 / 3.6
+        future_angle = float(vehicle_root[end_frame].get('yaw')) - float(vehicle_root[i].get('yaw'))
         if dist_buf > 50:
             break
 
@@ -79,16 +84,17 @@ def getVehicleDirection(vehicle_root, start_frame, end_frame):
     else:
         return 'straight'
 
+
 def getAnchor(track, start_frame, end_frame, crop_value, crop_rate):
     anchor_list = []
     for frame in range(start_frame, end_frame+1):
         for box in track.iter("box"):
-            if int(box.attrib.get("frame")) == frame:
+            if int(box.get("frame")) == frame:
                 anchor = [
-                    int((float(box.attrib.get('xbr')) - crop_value[2]) * (1 / crop_rate)),
-                    int((float(box.attrib.get('xtl')) - crop_value[2]) * (1 / crop_rate)),
-                    int((float(box.attrib.get('ybr')) - crop_value[0]) * (1 / crop_rate)),
-                    int((float(box.attrib.get('ytl')) - crop_value[0]) * (1 / crop_rate)),
+                    int((float(box.get('xbr')) - crop_value[2]) * (1 / crop_rate)),
+                    int((float(box.get('xtl')) - crop_value[2]) * (1 / crop_rate)),
+                    int((float(box.get('ybr')) - crop_value[0]) * (1 / crop_rate)),
+                    int((float(box.get('ytl')) - crop_value[0]) * (1 / crop_rate)),
                     ]
                 break
         anchor_list.append(anchor)
@@ -97,11 +103,11 @@ def getAnchor(track, start_frame, end_frame, crop_value, crop_rate):
 
 
 result_file_list = [
-    "/home/kuriatsu/Documents/data/pie_predict/result_0-150.pkl",
-    "/home/kuriatsu/Documents/data/pie_predict/result_151-300.pkl",
-    "/home/kuriatsu/Documents/data/pie_predict/result_301-450.pkl",
-    "/home/kuriatsu/Documents/data/pie_predict/result_451-600.pkl",
-    "/home/kuriatsu/Documents/data/pie_predict/result_601-719.pkl",
+    "/run/media/kuriatsu/SamsungKURI/PIE_data/extracted_data/predict/test/result_0-150.pkl",
+    "/run/media/kuriatsu/SamsungKURI/PIE_data/extracted_data/predict/test/result_151-300.pkl",
+    "/run/media/kuriatsu/SamsungKURI/PIE_data/extracted_data/predict/test/result_301-450.pkl",
+    "/run/media/kuriatsu/SamsungKURI/PIE_data/extracted_data/predict/test/result_451-600.pkl",
+    "/run/media/kuriatsu/SamsungKURI/PIE_data/extracted_data/predict/test/result_601-719.pkl",
     ]
 
 # load prediction result
@@ -118,38 +124,37 @@ for buf in prediction_data:
 set_list = ["set03"]
 
 video_list = [
-    "video_0001",
-    "video_0002",
-    "video_0003",
-    "video_0004",
-    "video_0005",
-    "video_0006",
-    "video_0007",
-    "video_0008",
-    "video_0009",
-    "video_0010",
-    "video_0011",
-    "video_0012",
-    "video_0013",
-    "video_0014",
-    "video_0015",
-    "video_0016",
-    "video_0017",
-    "video_0018",
-    "video_0019",
+    "set03/video_0001",
+    "set03/video_0002",
+    "set03/video_0003",
+    "set03/video_0004",
+    "set03/video_0005",
+    "set03/video_0006",
+    "set03/video_0007",
+    "set03/video_0008",
+    "set03/video_0009",
+    "set03/video_0010",
+    "set03/video_0011",
+    "set03/video_0012",
+    "set03/video_0013",
+    "set03/video_0014",
+    "set03/video_0015",
+    "set03/video_0016",
+    "set03/video_0017",
+    "set03/video_0018",
+    "set03/video_0019",
 ]
 
-int_length_list = [1.0, 3.0, 5.0, 7.0 ,9.0]
+
+base_dir = "/run/media/kuriatsu/SamsungKURI/PIE_data"
+annt_attribute_root = getXmlRoot("{}/annotations_attributes/{}_attributes.xml".format(base_dir, video_list[0]))
+annt_root = getXmlRoot("{}/annotations/{}_annt.xml".format(base_dir, video_list[0]))
+ego_vehicle_root = getXmlRoot("{}/annotations_vehicle/{}_obd.xml".format(base_dir, video_list[0]))
+video_file = "{}/PIE_clips/{}.mp4".format(base_dir, video_list[0])
 image_offset_y = 0.2
 crop_rate =  0.6
-video, _, _, crop_value = PieLib.getVideo(video_file, image_offset_y, crop_rate)
-image_res = [int(video.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(video.get(cv2.CAP_PROP_FRAME_WIDTH))]
-frame_rate = int(video.get(cv2.CAP_PROP_FPS))
+video, _, _, crop_value = getVideo(video_file, image_offset_y, crop_rate)
 expand_rate = 1.0 / crop_rate
-
-annt_attribute_root = getXmlRoot("/home/kuriatsu/media/SamsungKURI/PIE_data/annotations_attributes/{}/{}_attributes.xml".format(set_list[0], video_list[0]))
-annt_root = getXmlRoot("/home/kuriatsu/Documents/data/annotations/{}/{}_annt.xml".format(set_list[0], video_list[0]))
-ego_vehicle_root = getXmlRoot("/home/kuriatsu/media/SamsungKURI/PIE_data/annotations_attributes/{}/{}_vehicle.xml".format(set_list[0], video_list[0]))
 
 # {"video_path" :
 #     {"id" : str,
@@ -159,37 +164,39 @@ ego_vehicle_root = getXmlRoot("/home/kuriatsu/media/SamsungKURI/PIE_data/annotat
 #      "results" : float,
 #      "anchor" : [[xbr, xtl, ybr, ytl],...],
 #      "future_traj" : str,
-#      "critical_point" : int,
+#      "critical_point" : float,
 #      "crossing_point" : int,
 #     }
 # }
 database = {}
+int_length_list = [1.0, 3.0, 5.0, 7.0 ,9.0]
 
 for track in annt_root.iter("track"):
-    if track.attrib.get("label") == "pedestrian":
-        for box_attrib in track[0].iter("box"):
+    if track.get("label") == "pedestrian":
+        for box_attrib in track[0].iter("attribute"):
             if box_attrib.get("name") == "id":
                 ped_id = box_attrib.text
-        ped_attrib = getAtrrib(annt_attribute_root, ped_id).attrib.get("critical_point"))
-        max_len = int(ped_attrib.attrib.get("critical_point")) - int(track[0].attrib.get("frame"))
+        ped_attrib = getAtrrib(annt_attribute_root, ped_id)
+        max_len = (int(ped_attrib.get("critical_point")) - int(track[0].get("frame")))/30
 
         # short video
         if int_length_list[0] > max_len:
-            video_name = "/home/kuriatsu/media/SamsungKURI/PIE_data/extracted_data/{}_{}_ped_{}_0.mp4".format(set_list[0], video_list[0], ped_id)
-            start_frame = int(track[0].attrib.get("frame"))
-            end_frame = int(track[-1].attrib.get("frame"))
-            cutVideo(video, start_frame, end_frame, video_name, image_res)
+            video_name = "{}/extracted_data/{}_ped_{}_0.mp4".format(base_dir, video_list[0], ped_id)
+            start_frame = int(track[0].get("frame"))
+            end_frame = int(track[-1].get("frame"))
+            cutVideo(video, start_frame, end_frame, video_name, crop_value, expand_rate)
+            print("short video : {} - {}s {}->{}".format(ped_id, max_len, start_frame, end_frame))
 
             video_database = {
                 "id" : ped_id,
                 "label" : "pedestrian",
                 "int_length" : int_length,
-                "prob" : ped_attrib.attrib.get("intention_prob"),
+                "prob" : ped_attrib.get("intention_prob"),
                 "results" : result_dict.get(ped_id),
                 "anchor" : getAnchor(track, start_frame, end_frame, crop_value, crop_rate),
                 "future_traj" : getVehicleDirection(ego_vehicle_root, start_frame, end_frame),
-                "critical_point" : int(ped_attrib.attrib.get("intention_prob")) - start_frame,
-                "crossing_point" : int(ped_attrib.attrib.get("crossing_point")) - start_frame,
+                "critical_point" : float(ped_attrib.get("critical_point")) - start_frame,
+                "crossing_point" : int(ped_attrib.get("crossing_point")) - start_frame,
             }
 
             database[video_name] = video_database
@@ -198,38 +205,39 @@ for track in annt_root.iter("track"):
         for int_length in int_length_list:
             if int_length > max_len:
                 break
-
-            video_name = "/home/kuriatsu/media/SamsungKURI/PIE_data/extracted_data/{}_{}_ped_{}_{}.mp4".format(set_list[0], video_list[0], ped_id, int_length)
-            start_frame = int(ped_attrib.attrib.get("critical_point")) - int_length * 30
-            end_frame = int(track[-1].attrib.get("frame"))
-            cutVideo(video, start_frame, end_frame, video_name, image_res)
+            video_name = "{}/extracted_data/{}_ped_{}_{}.mp4".format(base_dir, video_list[0], ped_id, int_length)
+            start_frame = int(int(ped_attrib.get("critical_point")) - int_length * 30)
+            end_frame = int(track[-1].get("frame"))
+            cutVideo(video, start_frame, end_frame, video_name, crop_value, expand_rate)
+            print("start cut :{}- {}s {}->{}".format(ped_id, int_length, start_frame, end_frame))
 
             video_database = {
                 "id" : ped_id,
                 "int_length" : int_length,
-                "prob" : ped_attrib.attrib.get("intention_prob"),
+                "prob" : ped_attrib.get("intention_prob"),
                 "results" : result_dict.get(ped_id),
                 "anchor" : getAnchor(track, start_frame, end_frame, crop_value, crop_rate),
                 "future_traj" : getVehicleDirection(ego_vehicle_root, start_frame, end_frame),
-                "critical_point" : int(ped_attrib.attrib.get("intention_prob")) - start_frame,
-                "crossing_point" : int(ped_attrib.attrib.get("crossing_point")) - start_frame,
+                "critical_point" : float(ped_attrib.get("critical_point")) - start_frame,
+                "crossing_point" : int(ped_attrib.get("crossing_point")) - start_frame,
             }
 
             database[video_name] = video_database
 
 
-    if track.attrib.get("label") == "traffic_light":
-        for box_attrib in track[0].iter("box"):
+    if track.get("label") == "traffic_light":
+        for box_attrib in track[0].iter("attribute"):
             if box_attrib.get("name") == "id":
                 tl_id = box_attrib.text
-        max_len = int(track[-1].attrib.get("frame")) - int(track[0].attrib.get("frame"))
+        max_len = (int(track[-1].get("frame")) - int(track[0].get("frame")))/30
 
         # short video
         if int_length_list[0] > max_len:
-            video_name = "/home/kuriatsu/media/SamsungKURI/PIE_data/extracted_data/{}_{}_tl_{}_0.mp4".format(set_list[0], video_list[0], tl_id)
-            start_frame = int(track[0].attrib.get("frame"))
-            end_frame = int(track[-1].attrib.get("frame"))
-            cutVideo(video, start_frame, end_frame, video_name, image_res)
+            video_name = "{}/extracted_data/{}_tl_{}_0.mp4".format(base_dir, video_list[0], tl_id)
+            start_frame = int(track[0].get("frame"))
+            end_frame = int(track[-1].get("frame"))
+            cutVideo(video, start_frame, end_frame, video_name, crop_value, expand_rate)
+            print("short video : {} - {}s {}->{}".format(tl_id, max_len, start_frame, end_frame))
 
             video_database = {
                 "id" : tl_id,
@@ -239,8 +247,8 @@ for track in annt_root.iter("track"):
                 "results" : random.random(),
                 "anchor" : getAnchor(track, start_frame, end_frame, crop_value, crop_rate),
                 "future_traj" : getVehicleDirection(ego_vehicle_root, start_frame, end_frame),
-                "critical_point" : int(track[-1].attrib.get('frame')) - start_frame,
-                "crossing_point" : int(track[-1].attrib.get('frame'))) - start_frame,
+                "critical_point" : float(track[-1].get('frame')) - start_frame,
+                "crossing_point" : int(track[-1].get('frame')) - start_frame,
             }
 
             database[video_name] = video_database
@@ -250,10 +258,11 @@ for track in annt_root.iter("track"):
             if int_length > max_len:
                 break
 
-            video_name = "/home/kuriatsu/media/SamsungKURI/PIE_data/extracted_data/{}_{}_tl_{}_{}.mp4".format(set_list[0], video_list[0], tl_id, int_length)
-            start_frame = int(track[0].attrib.get("frame")) - int_length * 30
-            end_frame = int(track[-1].attrib.get("frame"))
-            cutVideo(video, start_frame, end_frame, video_name, image_res)
+            video_name = "{}/extracted_data/{}_tl_{}_{}.mp4".format(base_dir, video_list[0], tl_id, int_length)
+            start_frame = int(int(track[-1].get("frame")) - int_length * 30)
+            end_frame = int(track[-1].get("frame"))
+            cutVideo(video, start_frame, end_frame, video_name, crop_value, expand_rate)
+            print("start cut :{}- {}s {}->{}".format(tl_id, int_length, start_frame, end_frame))
 
             video_database = {
                 "id" : tl_id,
@@ -262,8 +271,11 @@ for track in annt_root.iter("track"):
                 "results" : random.random(),
                 "anchor" : getAnchor(track, start_frame, end_frame, crop_value, crop_rate),
                 "future_traj" : getVehicleDirection(ego_vehicle_root, start_frame, end_frame),
-                "critical_point" : int(track[-1].attrib.get('frame')) - start_frame,
-                "crossing_point" : int(track[-1].attrib.get('frame'))) - start_frame,
+                "critical_point" : float(track[-1].get('frame')) - start_frame,
+                "crossing_point" : int(track[-1].get('frame')) - start_frame,
             }
 
             database[video_name] = video_database
+
+with open("{}/extracted_data/database.pkl".format(base_dir), "wb") as f:
+    pickle.dump(database, f)
--- predict_analyze.py
+++ predict_analyze.py
@@ -15,32 +15,45 @@ def getXmlRoot(filename):
 
 
 result_file_list = [
-    "/home/kuriatsu/Documents/data/pie_predict/result_0-150.pkl",
-    "/home/kuriatsu/Documents/data/pie_predict/result_151-300.pkl",
-    "/home/kuriatsu/Documents/data/pie_predict/result_301-450.pkl",
-    "/home/kuriatsu/Documents/data/pie_predict/result_451-600.pkl",
-    "/home/kuriatsu/Documents/data/pie_predict/result_601-719.pkl",
+    # "/home/kuriatsu/Documents/data/pie_predict/val/result_0-150.pkl",
+    # "/home/kuriatsu/Documents/data/pie_predict/val/result_151-243.pkl",
+    "/home/kuriatsu/Documents/data/pie_predict/test/result_0-150.pkl",
+    "/home/kuriatsu/Documents/data/pie_predict/test/result_151-300.pkl",
+    "/home/kuriatsu/Documents/data/pie_predict/test/result_301-450.pkl",
+    "/home/kuriatsu/Documents/data/pie_predict/test/result_451-600.pkl",
+    "/home/kuriatsu/Documents/data/pie_predict/test/result_601-719.pkl",
     ]
 video_list = [
-    "video_0001",
-    "video_0002",
-    "video_0003",
-    "video_0004",
-    "video_0005",
-    "video_0006",
-    "video_0007",
-    "video_0008",
-    "video_0009",
-    "video_0010",
-    "video_0011",
-    "video_0012",
-    "video_0013",
-    "video_0014",
-    "video_0015",
-    "video_0016",
-    "video_0017",
-    "video_0018",
-    "video_0019",
+    # "set05/video_0001",
+    # "set05/video_0002",
+    # "set06/video_0001",
+    # "set06/video_0002",
+    # "set06/video_0003",
+    # "set06/video_0004",
+    # "set06/video_0005",
+    # "set06/video_0006",
+    # "set06/video_0007",
+    # "set06/video_0008",
+    # "set06/video_0009",
+    "set03/video_0001",
+    "set03/video_0002",
+    "set03/video_0003",
+    "set03/video_0004",
+    "set03/video_0005",
+    "set03/video_0006",
+    "set03/video_0007",
+    "set03/video_0008",
+    "set03/video_0009",
+    "set03/video_0010",
+    "set03/video_0011",
+    "set03/video_0012",
+    "set03/video_0013",
+    "set03/video_0014",
+    "set03/video_0015",
+    "set03/video_0016",
+    "set03/video_0017",
+    "set03/video_0018",
+    "set03/video_0019",
 ]
 
 # load prediction result
@@ -61,25 +74,38 @@ for buf in data:
 # get annotation
 annotation_len_list = []
 exp_len_list = []
+annotation_len_list_tl = []
 for video in video_list:
-    annt = getXmlRoot("/home/kuriatsu/Documents/data/annotations/{}/{}_annt.xml".format("set03", video))
-    attrib = getXmlRoot("/home/kuriatsu/Documents/data/annotations_attributes/{}/{}_attributes.xml".format("set03", video))
+    annt = getXmlRoot("/home/kuriatsu/Documents/data/annotations/{}_annt.xml".format(video))
+    attrib = getXmlRoot("/home/kuriatsu/Documents/data/annotations_attributes/{}_attributes.xml".format(video))
 
     for track in annt.iter("track"):
         if track.attrib.get("label") == "pedestrian":
-            annotation_len_list.append((int(track[-1].attrib.get("frame")) - int(track[0].attrib.get("frame")))/30)
+            for attribute in track[0].iter("attribute"):
+                if attribute.get("name") == "id":
+                    ped_id = attribute.text
+                    break
 
-    for pedestrian in attrib.iter("pedestrian"):
-        exp_len_list.append((int(pedestrian.attrib.get("critical_point"))-int(pedestrian.attrib.get("exp_start_point")))/30)
-        result_dict.at[pedestrian.attrib.get("id"), "prob"] = float(pedestrian.attrib.get("intention_prob"))
+            for pedestrian in attrib.iter("pedestrian"):
+                if pedestrian.get("id") == ped_id:
+                    exp_len_list.append((int(pedestrian.attrib.get("critical_point"))-int(pedestrian.attrib.get("exp_start_point")))/30)
+                    result_dict.at[pedestrian.attrib.get("id"), "prob"] = float(pedestrian.attrib.get("intention_prob"))
+                    annotation_len_list.append((int(pedestrian.attrib.get("critical_point")) - int(track[0].attrib.get("frame")))/30)
+                    break
 
+        if track.attrib.get("label") == "traffic_light":
+            annotation_len_list_tl.append((int(track[-1].attrib.get("frame")) - int(track[0].attrib.get("frame")))/30)
+
+            break
     del annt
     del attrib
 
 # visualize annotation length
 fig, axes = plt.subplots()
-sns.histplot(annotation_len_list, binwidth=1, color="tomato", ax=axes)
-sns.histplot(exp_len_list, binwidth=1, color="steelblue", ax=axes)
+sns.histplot(annotation_len_list, binwidth=1, color="tomato", ax=axes, label="max_int_time")
+sns.histplot(exp_len_list, binwidth=1, color="steelblue", ax=axes, label="exp_time")
+sns.histplot(annotation_len_list_tl, binwidth=1, color="seagreen", ax=axes, label="max_int_time(tl)")
+plt.legend()
 plt.show()
 
 # visualize prediction result
@@ -88,13 +114,20 @@ sns.histplot(data=melted_df, binwidth=0.05, x="value", hue="type")
 plt.show()
 
 # calc accuracy
-acc_list = {}
+fp_list = {}
+fn_list = {}
 
-for thres in range(0.0, 1.0, 0.1):
-    correct_count = 0
-    for result in result_dict.iterrows():
-        correct_count+=(result[1]["prob"] < thres and result[1]["result"] < thres) or (result[1]["prob"] >= thres and result[1]["result"] >= thres)
-    acc_list[thres] = correct_count/len(result_dict.index)
+for thres in range(0, 11):
+    fp_count = 0
+    fn_count = 0
+    for index, result in result_dict.iterrows():
+        fn_count+=(result["prob"] >= thres*0.1 and result["result"] < thres*0.1)
+        fp_count+=(result["prob"] < thres*0.1 and result["result"] >= thres*0.1)
+    fp_list[thres*0.1] = fp_count/len(result_dict.index)
+    fn_list[thres*0.1] = fn_count/len(result_dict.index)
 
-sns.plot(x=acc_list.keys(), y=acc_list.values())
+fig, axes = plt.subplots()
+sns.lineplot(x=fp_list.keys(), y=fp_list.values(), color="tomato", label="false-positive", ax=axes)
+sns.lineplot(x=fn_list.keys(), y=fn_list.values(), color="steelblue", label="false-negative", ax=axes)
+plt.legend()
 plt.show()
