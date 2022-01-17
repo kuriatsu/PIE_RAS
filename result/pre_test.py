e1035ccf8ff18ec0988731f55c2c1a1a0940a859
kuriatsu <kuriatsubayashi712@gmail.com>
2022-01-17 21:01:46 +0900

    add plot

--- result/pre_test.py
+++ result/pre_test.py
@@ -5,6 +5,9 @@ import pickle
 import pandas as pd
 import xml.etree.ElementTree as ET
 import math
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
 
 def getXmlRoot(filename):
 
@@ -20,7 +23,9 @@ def getAtrrib(root, id):
     return None
 
 
-base_dir = "/home/kuriatsu/Documents/experiment/pie"
+base_dir = "/media/kuriatsu/SamsungKURI1/PIE_data"
+# base_dir = "/home/kuriatsu/Documents/experiment/pie"
+
 video_list = [
     "set03/video_0001",
     "set03/video_0002",
@@ -45,15 +50,24 @@ video_list = [
     # "set05/video_0002",
 ]
 
-summary = pd.DataFrame(columns=["id", "thres", "last_state", "int_result", "int_time", "prediction_value", "prediction_bin", "annotation_value", "annotation_bin", "crossing"])
+play_list = pd.read_csv("{}/extracted_data/playlist.csv".format(base_dir))
+summary = pd.DataFrame(columns=["id", "thres", "last_state", "int_result", "int_time", "prediction_value", "prediction_bin", "annotation_value", "annotation_bin", "crossing", "limit"])
 summary_list = []
-data = pd.read_csv("{}/202201_pre_experiment/log_data_1_kanayama_nocon.csv".format(base_dir))
+# data = pd.read_csv("{}/202201_pre_experiment/log_data_1_kanayama_nocon.csv".format(base_dir))
+data = pd.read_csv("{}/extracted_data/log_data_1_kanayama_nocon.csv".format(base_dir))
 for i, row in data.iterrows():
-    summary_list.append([row.id, 0.5, row.last_state, 0, row.last_int_time, 0, 0, 0, 0, 0])
+    for video_name in play_list.iloc[0, :]:
+        if video_name.startswith(row.id):
+            int_time = video_name.split("_")[-1]
+    summary_list.append([row.id, 0.5, row.last_state, 0, float(row.last_int_frame)/30.0, 0, 0, 0, 0, 0, float(int_time)])
 
-data = pd.read_csv("{}/202201_pre_experiment/log_data_2_kanayama_con.csv".format(base_dir))
+# data = pd.read_csv("{}/202201_pre_experiment/log_data_2_kanayama_con.csv".format(base_dir))
+data = pd.read_csv("{}/extracted_data/log_data_2_kanayama_con.csv".format(base_dir))
 for i, row in data.iterrows():
-    summary_list.append([row.id, 0.0, row.last_state, 0, row.last_int_time, 0, 0, 0, 0, 0])
+    for video_name in play_list.iloc[1, :]:
+        if video_name.startswith(row.id):
+            int_time = video_name.split("_")[-1]
+    summary_list.append([row.id, 0.0, row.last_state, 0, float(row.last_int_frame)/30.0, 0, 0, 0, 0, 0, float(int_time)])
 
 for video_name in video_list:
     root = getXmlRoot("{}/annotations_attributes/{}_attributes.xml".format(base_dir, video_name))
@@ -66,11 +80,11 @@ for video_name in video_list:
                 row[9] = 0 if row[9] == -1 else row[9]
 
 result_file_list = [
-    base_dir + "/pie_predict/test/result_0-150.pkl",
-    base_dir + "/pie_predict/test/result_151-300.pkl",
-    base_dir + "/pie_predict/test/result_301-450.pkl",
-    base_dir + "/pie_predict/test/result_451-600.pkl",
-    base_dir + "/pie_predict/test/result_601-719.pkl",
+    base_dir + "/extracted_data/predict/test/result_0-150.pkl",
+    base_dir + "/extracted_data/predict/test/result_150-300.pkl",
+    base_dir + "/extracted_data/predict/test/result_300-450.pkl",
+    base_dir + "/extracted_data/predict/test/result_450-600.pkl",
+    base_dir + "/extracted_data/predict/test/result_600-719.pkl",
     # base_dir + "/extracted_data/predict/val/result_0-150.pkl",
     # base_dir + "/extracted_data/predict/val/result_151-243.pkl",
     ]
@@ -141,3 +155,79 @@ for row in summary_list:
     # else:
     #     row[9] = 1
     #     row[8] = 1 if row[2] == False else 0
+# print(summary_list)
+total_acc = [
+[row[3] for row in summary_list if row[10]==0.0].count(1)/len([row[3] for row in summary_list if row[10]==0.0]),
+[row[3] for row in summary_list if row[10]==1.0].count(1)/len([row[3] for row in summary_list if row[10]==1.0]),
+[row[3] for row in summary_list if row[10]==3.0].count(1)/len([row[3] for row in summary_list if row[10]==3.0]),
+[row[3] for row in summary_list if row[10]==5.0].count(1)/len([row[3] for row in summary_list if row[10]==5.0]),
+[row[3] for row in summary_list if row[10]==7.0].count(1)/len([row[3] for row in summary_list if row[10]==7.0]),
+[row[3] for row in summary_list if row[10]==9.0].count(1)/len([row[3] for row in summary_list if row[10]==9.0]),
+]
+print("int limit time and acc", total_acc)
+time_acc = [
+len([row[3] for row in summary_list if (row[10]==0.0 and not math.isnan(row[4]))]),
+len([row[3] for row in summary_list if (row[10]==1.0 and not math.isnan(row[4]))]),
+len([row[3] for row in summary_list if (row[10]==3.0 and not math.isnan(row[4]))]),
+len([row[3] for row in summary_list if (row[10]==5.0 and not math.isnan(row[4]))]),
+len([row[3] for row in summary_list if (row[10]==7.0 and not math.isnan(row[4]))]),
+len([row[3] for row in summary_list if (row[10]==9.0 and not math.isnan(row[4]))]),
+]
+print("intervention count",time_acc)
+int_acc = [
+[row[3] for row in summary_list if (row[10]==0.0 and not math.isnan(row[4]))].count(1)/len([row[3] for row in summary_list if (row[10]==0.0 and not math.isnan(row[4]))]),
+[row[3] for row in summary_list if (row[10]==1.0 and not math.isnan(row[4]))].count(1)/len([row[3] for row in summary_list if (row[10]==1.0 and not math.isnan(row[4]))]),
+[row[3] for row in summary_list if (row[10]==3.0 and not math.isnan(row[4]))].count(1)/len([row[3] for row in summary_list if (row[10]==3.0 and not math.isnan(row[4]))]),
+[row[3] for row in summary_list if (row[10]==5.0 and not math.isnan(row[4]))].count(1)/len([row[3] for row in summary_list if (row[10]==5.0 and not math.isnan(row[4]))]),
+[row[3] for row in summary_list if (row[10]==7.0 and not math.isnan(row[4]))].count(1)/len([row[3] for row in summary_list if (row[10]==7.0 and not math.isnan(row[4]))]),
+[row[3] for row in summary_list if (row[10]==9.0 and not math.isnan(row[4]))].count(1)/len([row[3] for row in summary_list if (row[10]==9.0 and not math.isnan(row[4]))]),
+]
+print("intervention acc", int_acc)
+cross_rate = [
+[row[8] for row in summary_list if row[10]==0.0].count(1)/len([row[8] for row in summary_list if row[10]==0.0]),
+[row[8] for row in summary_list if row[10]==1.0].count(1)/len([row[8] for row in summary_list if row[10]==1.0]),
+[row[8] for row in summary_list if row[10]==3.0].count(1)/len([row[8] for row in summary_list if row[10]==3.0]),
+[row[8] for row in summary_list if row[10]==5.0].count(1)/len([row[8] for row in summary_list if row[10]==5.0]),
+[row[8] for row in summary_list if row[10]==7.0].count(1)/len([row[8] for row in summary_list if row[10]==7.0]),
+[row[8] for row in summary_list if row[10]==9.0].count(1)/len([row[8] for row in summary_list if row[10]==9.0]),
+]
+print("annot cross", cross_rate)
+
+pred_acc = [
+[row[6] for row in summary_list if row[10]==0.0].count(1)/len([row[6] for row in summary_list if row[10]==0.0]),
+[row[6] for row in summary_list if row[10]==1.0].count(1)/len([row[6] for row in summary_list if row[10]==1.0]),
+[row[6] for row in summary_list if row[10]==3.0].count(1)/len([row[6] for row in summary_list if row[10]==3.0]),
+[row[6] for row in summary_list if row[10]==5.0].count(1)/len([row[6] for row in summary_list if row[10]==5.0]),
+[row[6] for row in summary_list if row[10]==7.0].count(1)/len([row[6] for row in summary_list if row[10]==7.0]),
+[row[6] for row in summary_list if row[10]==9.0].count(1)/len([row[6] for row in summary_list if row[10]==9.0]),
+]
+print("prediction cross", pred_acc)
+
+time_acc = [
+[row[6] == row[8] for row in summary_list if row[10]==0.0].count(1)/len([row[6] for row in summary_list if row[10]==0.0]),
+[row[6] == row[8] for row in summary_list if row[10]==1.0].count(1)/len([row[6] for row in summary_list if row[10]==1.0]),
+[row[6] == row[8] for row in summary_list if row[10]==3.0].count(1)/len([row[6] for row in summary_list if row[10]==3.0]),
+[row[6] == row[8] for row in summary_list if row[10]==5.0].count(1)/len([row[6] for row in summary_list if row[10]==5.0]),
+[row[6] == row[8] for row in summary_list if row[10]==7.0].count(1)/len([row[6] for row in summary_list if row[10]==7.0]),
+[row[6] == row[8] for row in summary_list if row[10]==9.0].count(1)/len([row[6] for row in summary_list if row[10]==9.0]),
+]
+print("prediction acc", time_acc)
+
+time_acc = [
+sum([row[4] for row in summary_list if (row[10]==0.0 and not math.isnan(row[4]))])/len([row[4] for row in summary_list if (row[10]==0.0 and not math.isnan(row[4]))]),
+sum([row[4] for row in summary_list if (row[10]==1.0 and not math.isnan(row[4]))])/len([row[4] for row in summary_list if (row[10]==1.0 and not math.isnan(row[4]))]),
+sum([row[4] for row in summary_list if (row[10]==3.0 and not math.isnan(row[4]))])/len([row[4] for row in summary_list if (row[10]==3.0 and not math.isnan(row[4]))]),
+sum([row[4] for row in summary_list if (row[10]==5.0 and not math.isnan(row[4]))])/len([row[4] for row in summary_list if (row[10]==5.0 and not math.isnan(row[4]))]),
+sum([row[4] for row in summary_list if (row[10]==7.0 and not math.isnan(row[4]))])/len([row[4] for row in summary_list if (row[10]==7.0 and not math.isnan(row[4]))]),
+sum([row[4] for row in summary_list if (row[10]==9.0 and not math.isnan(row[4]))])/len([row[4] for row in summary_list if (row[10]==9.0 and not math.isnan(row[4]))]),
+]
+print("intervention time", time_acc)
+
+left = np.arange(len(int_acc))
+fig, ax = plt.subplots()
+ax.bar(x=left, height=int_acc, width=0.4, align="center", label="intervention")
+ax.bar(x=left+0.4, height=pred_acc, width=0.4, align="center", label="prediction")
+ax.bar(x=left+0.8, height=total_acc, width=0.4, align="center", label="total")
+ax.set_xticks(left+0.2)
+ax.set_xticklabels(labels=[0.0, 1.0, 3.0, 5.0, 7.0, 9.0])
+plt.show()
