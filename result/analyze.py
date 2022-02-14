096d3253d266615392853548d80cbb25dcaf3e06
kuriatsu <kuriatsubayashi712@gmail.com>
2022-02-14 18:51:28 +0900

    finish data collection

--- result/analyze.py
+++ result/analyze.py
@@ -9,51 +9,117 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import numpy as np
 
-with open("/home/kuriatsu/Documents/experiment/pie/202201experiment/database_result_valid.pkl", "rb") as f:
-    database = pickle.load(f)
+database = pd.read_csv("/media/kuriatsu/SamsungKURI/PIE_data/extracted_data/log/summary.csv")
 
-base_dir = "/home/kuriatsu/Documents/experiment/pie/202201experiment/"
-result_list = [
-    "{}/log_data_takanose_0.0.csv".format(base_dir),
-    "{}/log_data_takanose_0.5.csv".format(base_dir),
-    "{}/log_data_takanose_0.8.csv".format(base_dir),
-    "{}/log_data_tyamamoto_0.0.csv".format(base_dir),
-    "{}/log_data_tyamamoto_0.5.csv".format(base_dir),
-    "{}/log_data_tyamamoto_0.8.csv".format(base_dir),
-]
-database.get("3_9_593_12.0").get("int_length")
-for i in range(1, len(result_list)):
-    result = result.append(pd.read_csv(result_list[i]))
-int_length_list = result.int_length.drop_duplicates()
-int_length_summary = pd.DataFrame(columns=["int_length", "acc", "int_thresh", "type", "count"])
-for i, row in result.iterrows():
-    if len(int_length_summary[(int_length_summary.int_length==row.int_length) & (int_length_summary.int_thresh == row.int_thresh)])  == 0:
-        for type in ["all", "intervention", "necessary"]:
-            buf  = pd.Series([row.int_length, 0, row.int_thresh, type, 0], index=int_length_summary.columns)
-            int_length_summary = int_length_summary.append(buf, ignore_index=True)
-
-    data = database.get(row.id + "_" + str(row.int_length))
-    if (data.get("prob")>0.5 and row.last_state == True) or (data.get("prob")<=0.5 and row.last_state == False):
-        int_length_summary[(int_length_summary.int_length==row.int_length) & (int_length_summary.int_thresh==row.int_thresh) & (int_length_summary.type=="all")]["acc"] += 1
-        int_length_summary[(int_length_summary.int_length==row.int_length) & (int_length_summary.int_thresh==row.int_thresh) & (int_length_summary.type=="all")]["count"] += 1
-
-        if not row.int_method.type:
-            int_length_summary[(int_length_summary.int_length==row.int_length) & (int_length_summary.int_thresh==row.int_thresh) & (int_length_summary.type=="intervention")].acc += 1
-            int_length_summary[(int_length_summary.int_length==row.int_length) & (int_length_summary.int_thresh==row.int_thresh) & (int_length_summary.type=="intervention")].count += 1
-
-        if (data.get("prob") > 0.5 and data.get("result") <= 0.5) or data.get("prob") <= 0.5 and data.get("result").get("prob") > 0.5:
-            int_length_summary[(int_length_summary.int_length==row.int_length) & (int_length_summary.int_thresh==row.int_thresh) & (int_length_summary.type=="necessary")].acc += 1
-            int_length_summary[(int_length_summary.int_length==row.int_length) & (int_length_summary.int_thresh==row.int_thresh) & (int_length_summary.type=="necessary")].count += 1
-    else:
-        int_length_summary[(int_length_summary.int_length==row.int_length) & (int_length_summary.int_thresh==row.int_thresh) & (int_length_summary.type=="all")].count += 1
-
-        if not math.isnan(row.int_method):
-            int_length_summary[(int_length_summary.int_length==row.int_length) & (int_length_summary.int_thresh==row.int_thresh) & (int_length_summary.type=="intervention")].count += 1
-
-        if (data.get("prob") > 0.5 and data.get("result") <= 0.5) or data.get("prob") <= 0.5 and data.get("result").get("prob") > 0.5:
-            int_length_summary[(int_length_summary.int_length==row.int_length) & (int_length_summary.int_thresh==row.int_thresh) & (int_length_summary.type=="necessary")].count += 1
+fig, axes = plt.subplots(1, 3)
+sns.countplot(x="int_acc_prob", hue="length", data=database[database.recognition_thresh==0.0], ax=axes[0])
+sns.countplot(x="int_acc_prob", hue="length", data=database[database.recognition_thresh==0.5], ax=axes[1])
+sns.countplot(x="int_acc_prob", hue="length", data=database[database.recognition_thresh==0.8], ax=axes[2])
 
 
-    acc = result[(result.int_length==int_length) & ((database.get(result.id).get("prob")>0.5))]
+cross_total = pd.DataFrame(columns=["thres", "type", "Hit", "Miss", "FA", "CR"])
+for thres in [0.0, 0.5, 0.8]:
+    buf = pd.Series([
+        thres,
+        "intervention",
+        (database[database.recognition_thresh==thres].int_acc_prob == 0).sum(),
+        (database[database.recognition_thresh==thres].int_acc_prob == 1).sum(),
+        (database[database.recognition_thresh==thres].int_acc_prob == 2).sum(),
+        (database[database.recognition_thresh==thres].int_acc_prob == 3).sum(),
+        ], index = cross_total.columns)
+    cross_total = cross_total.append(buf, ignore_index=True)
 
-len(int_length_summary[(int_length_summary.int_length==1.0) & (int_length_summary.int_thresh == 0.8)])
+    buf = pd.Series([
+        thres,
+        "prediction",
+        (database[database.recognition_thresh==thres].prediction_acc == 0).sum(),
+        (database[database.recognition_thresh==thres].prediction_acc == 1).sum(),
+        (database[database.recognition_thresh==thres].prediction_acc == 2).sum(),
+        (database[database.recognition_thresh==thres].prediction_acc == 3).sum(),
+        ], index = cross_total.columns)
+    cross_total = cross_total.append(buf, ignore_index=True)
+
+cross_total_acc_rate = pd.DataFrame(columns=["val", "type", "thres"])
+for type in ["intervention", "prediction"]:
+    for thres in [0.0, 0.5, 0.8]:
+        val = (cross_total[(cross_total.thres==thres) & (cross_total.type==type)].Hit + cross_total[(cross_total.thres==thres) & (cross_total.type==type)].CR) / (cross_total[(cross_total.thres==thres) & (cross_total.type==type)].Hit + cross_total[(cross_total.thres==thres) & (cross_total.type==type)].Miss + cross_total[(cross_total.thres==thres) & (cross_total.type==type)].FA + cross_total[(cross_total.thres==thres) & (cross_total.type==type)].CR)
+        buf = pd.Series([val, type, thres], index=cross_total_acc_rate.columns)
+        cross_total_acc_rate = cross_total_acc_rate.append(buf, ignore_index=True)
+
+cross_length_acc_rate = pd.DataFrame(columns=["val", "type", "thres"])
+for thres in [0.0, 0.5, 0.8]:
+    val = (cross_total[(cross_total.thres==thres) & (cross_total.type==type)].Hit + cross_total[(cross_total.thres==thres) & (cross_total.type==type)].CR) / (cross_total[(cross_total.thres==thres) & (cross_total.type==type)].Hit + cross_total[(cross_total.thres==thres) & (cross_total.type==type)].Miss + cross_total[(cross_total.thres==thres) & (cross_total.type==type)].FA + cross_total[(cross_total.thres==thres) & (cross_total.type==type)].CR)
+    buf = pd.Series([val, type, thres], index=cross_total_acc_rate.columns)
+    cross_total_acc_rate = cross_total_acc_rate.append(buf, ignore_index=True)
+
+acc_length = pd.DataFrame(columns=["type", "count", "length", "thres"])
+for thres in [0.0, 0.5, 0.8]:
+    for length in database.length.unique():
+        for i, type in enumerate(["Hit", "Miss", "FA", "CR"]):
+            count = (database[(database.recognition_thresh==thres) & (database.length == length)].int_acc_prob == i).sum()
+            buf = pd.Series([type, count, length, thres], index = acc_length.columns)
+            acc_length = acc_length.append(buf, ignore_index=True)
+
+sns.barplot(x="type", y="count", hue="length", data=acc_length[acc_length.thres == 0.0])
+sns.barplot(x="type", y="count", hue="length", data=acc_length[acc_length.thres == 0.5])
+sns.barplot(x="type", y="count", hue="length", data=acc_length[acc_length.thres == 0.8])
+
+
+intv_count_length = pd.DataFrame(columns=["type", "count", "length", "thres"])
+for thres in [0.0, 0.5, 0.8]:
+    for length in database.length.unique():
+        count = len(database[(database.recognition_thresh==thres) & (database.length == length) & (database.int_count > 0)])
+        buf = pd.Series(["intervention", count, length, thres], index = intv_count_length.columns)
+        intv_count_length = intv_count_length.append(buf, ignore_index=True)
+
+        count = len(database[(database.recognition_thresh==thres) & (database.length == length) & ((database.prediction_acc==1) | (database.prediction_acc==2))])
+        buf = pd.Series(["necessary", count, length, thres], index = intv_count_length.columns)
+        intv_count_length = intv_count_length.append(buf, ignore_index=True)
+
+sns.barplot(x="length", y="count", hue="type", data=intv_count_length[intv_count_length.thres == 0.0])
+sns.barplot(x="length", y="count", hue="type", data=intv_count_length[intv_count_length.thres == 0.5])
+sns.barplot(x="length", y="count", hue="type", data=intv_count_length[intv_count_length.thres == 0.8])
+
+intv_acc_length = pd.DataFrame(columns=["type", "count", "length", "thres"])
+for thres in [0.0, 0.5, 0.8]:
+    for length in database.length.unique():
+        for i, type in enumerate(["Hit", "Miss", "FA", "CR"]):
+            count = (database[(database.recognition_thresh==thres) & (database.length == length) & (database.int_count > 0)].int_acc_prob == i).sum()
+            buf = pd.Series([type, count, length, thres], index = intv_acc_length.columns)
+            intv_acc_length = intv_acc_length.append(buf, ignore_index=True)
+
+sns.barplot(x="type", y="count", hue="length", data=intv_acc_length[intv_acc_length.thres == 0.0])
+sns.barplot(x="type", y="count", hue="length", data=intv_acc_length[intv_acc_length.thres == 0.5])
+sns.barplot(x="type", y="count", hue="length", data=intv_acc_length[intv_acc_length.thres == 0.8])
+
+
+intention_count_data = pd.DataFrame(columns=["Hit", "Miss", "FA", "CR", "length", "thres"])
+for thres in [0.0, 0.5, 0.8]:
+    buf = pd.Series([
+        (database[database.recognition_thresh==thres].int_acc_prediction == 0).sum(),
+        (database[database.recognition_thresh==thres].int_acc_prediction == 1).sum(),
+        (database[database.recognition_thresh==thres].int_acc_prediction == 2).sum(),
+        (database[database.recognition_thresh==thres].int_acc_prediction == 3).sum(),
+        "total",
+        thres,
+        ], index = acc_count_data.columns)
+    intention_count_data = acc_count_data.append(buf, ignore_index=True)
+
+    for length in database.length.unique():
+        buf = pd.Series([
+            (database[(database.recognition_thresh==thres) & (database.length == length)].int_acc_prediction == 0).sum(),
+            (database[(database.recognition_thresh==thres) & (database.length == length)].int_acc_prediction == 1).sum(),
+            (database[(database.recognition_thresh==thres) & (database.length == length)].int_acc_prediction == 2).sum(),
+            (database[(database.recognition_thresh==thres) & (database.length == length)].int_acc_prediction == 3).sum(),
+            length,
+            thres,
+            ], index = acc_count_data.columns)
+        intention_count_data = acc_count_data.append(buf, ignore_index=True)
+
+int_length_data = pd.DataFrame(columns=[1.0, 3.0, 5.0, 7.0, 9.0, 12.0])
+for length in int_length_data.columns:
+    index = float(length)
+    target = database[database.length==index]
+    int_length_data.loc["intervention_total", index] = len(target[((target.result_acc == 0)|(target.result_acc == 3))]) / len(target)
+    int_length_data.loc["prediction", index] = len(target[((target.prediction_prob > 0.5) & (target.annt_prob > 0.5)) | ((target.prediction_prob <= 0.5) & (target.annt_prob <= 0.5))]) / len(target)
+    int_length_data.loc["total", index] = len(target[((target.last_intention==True) & (target.annt_prob<=0.5)) | ((target.last_intention==False) & (target.annt_prob>0.5))]) / len(target)
--- result/data_collection.py
+++ result/data_collection.py
@@ -14,11 +14,13 @@ import seaborn as sns
 
 # get data
 log_data = None
-data_path = "/home/kuriatsu/Documents/experiment/pie_202201"
+data_path = "/media/kuriatsu/SamsungKURI/PIE_data/extracted_data/log"
 for file in glob.glob(os.path.join(data_path, "log*.csv")):
     buf = pd.read_csv(file)
     filename =file.split("/")[-1]
     subject = filename.rsplit("_", 1)[0].replace("log_data_", "")
+    if subject in ["kanayama"]:
+        break
     trial = filename.split("_")[-1].replace(".csv", "")
     buf["subject"] = subject
     buf["trial"] = trial
@@ -27,7 +29,7 @@ for file in glob.glob(os.path.join(data_path, "log*.csv")):
     else:
         log_data = log_data.append(buf, ignore_index=True)
 
-with open("/home/kuriatsu/Documents/experiment/pie_202201/database_result_valid_cross.pkl", "rb") as f:
+with open("/media/kuriatsu/SamsungKURI/PIE_data/extracted_data/database_result_valid_cross.pkl", "rb") as f:
     database_valid = pickle.load(f)
 
 # with open("/media/kuriatsu/SamsungKURI/PIE_data/extracted_data/database.pkl", "rb") as f:
@@ -46,9 +48,9 @@ extracted_data = pd.DataFrame(columns=[
                                 "prediction_prob",
                                 "last_intention", # False=cross True=no_cross
                                 "crossing",
-                                "int_acc", # 0=hit, 1=miss, 2=FA, 3=CR
-                                "int_cross_acc",
-                                "prediction_cross_acc",
+                                "int_acc_prediction", # 0=hit, 1=miss, 2=FA, 3=CR
+                                "int_acc_prob",
+                                "prediction_acc",
                                 ])
 
 for i, row in log_data.iterrows():
@@ -80,12 +82,12 @@ for i, row in log_data.iterrows():
         float(database_valid.get(data_id).get("prob")),
         float(database_valid.get(data_id).get("results")),
         row.last_state,
-        None, # database_valid.get(data_id).get("crossing"),
+        int(database_valid.get(data_id).get("crossing")),
         None,
         None,
         None,
         ], index=extracted_data.columns)
-
+    # print(database_valid.get(data_id).get("crossing"))
     # if row.subject in ["tyamamoto", "takanose"]:
     #     buf["intention_prob"] = database.get(data_id).get("prob")
     #     buf["prediction_res"] = database.get(data_id).get("results")
@@ -96,158 +98,52 @@ for i, row in log_data.iterrows():
 
     if buf.prediction_prob > buf.recognition_thresh and buf.annt_prob > 0.5:
         if not buf.last_intention:
-            buf.int_acc = 3
+            buf.int_acc_prediction = 3
         else:
-            buf.int_acc = 2
+            buf.int_acc_prediction = 2
 
     elif buf.prediction_prob <= buf.recognition_thresh and buf.annt_prob <= 0.5:
         if buf.last_intention:
-            buf.int_acc = 3
+            buf.int_acc_prediction = 3
         else:
-            buf.int_acc = 2
+            buf.int_acc_prediction = 2
 
     elif buf.prediction_prob > buf.recognition_thresh and buf.annt_prob <= 0.5:
         if buf.last_intention:
-            buf.int_acc = 0
+            buf.int_acc_prediction = 0
         else:
-            buf.int_acc = 1
+            buf.int_acc_prediction = 1
 
     elif buf.prediction_prob <= buf.recognition_thresh and buf.annt_prob > 0.5:
         if not buf.last_intention:
-            buf.int_acc = 0
+            buf.int_acc_prediction = 0
         else:
-            buf.int_acc = 1
+            buf.int_acc_prediction = 1
 
     if buf.annt_prob <= 0.5:
         if buf.last_intention:
-            buf.int_cross_acc = 3
+            buf.int_acc_prob = 3
         else:
-            buf.int_cross_acc = 2
+            buf.int_acc_prob = 2
 
     elif  buf.annt_prob > 0.5:
         if not buf.last_intention:
-            buf.int_cross_acc = 0
+            buf.int_acc_prob = 0
         else:
-            buf.int_cross_acc = 1
+            buf.int_acc_prob = 1
 
     if buf.annt_prob <= 0.5:
         if buf.prediction_prob <= buf.recognition_thresh:
-            buf.prediction_cross_acc = 3
+            buf.prediction_acc = 3
         else:
-            buf.prediction_cross_acc = 2
+            buf.prediction_acc = 2
 
     elif  buf.annt_prob > 0.5:
         if buf.prediction_prob > buf.recognition_thresh:
-            buf.prediction_cross_acc = 0
+            buf.prediction_acc = 0
         else:
-            buf.prediction_cross_acc = 1
+            buf.prediction_acc = 1
 
     extracted_data = extracted_data.append(buf, ignore_index=True)
 
-
-cross_total = pd.DataFrame(columns=["thres", "type", "Hit", "Miss", "FA", "CR"])
-for thres in [0.0, 0.5, 0.8]:
-    buf = pd.Series([
-        thres,
-        "intervention",
-        (extracted_data[extracted_data.recognition_thresh==thres].int_cross_acc == 0).sum(),
-        (extracted_data[extracted_data.recognition_thresh==thres].int_cross_acc == 1).sum(),
-        (extracted_data[extracted_data.recognition_thresh==thres].int_cross_acc == 2).sum(),
-        (extracted_data[extracted_data.recognition_thresh==thres].int_cross_acc == 3).sum(),
-        ], index = cross_total.columns)
-    cross_total = cross_total.append(buf, ignore_index=True)
-
-    buf = pd.Series([
-        thres,
-        "prediction",
-        (extracted_data[extracted_data.recognition_thresh==thres].prediction_cross_acc == 0).sum(),
-        (extracted_data[extracted_data.recognition_thresh==thres].prediction_cross_acc == 1).sum(),
-        (extracted_data[extracted_data.recognition_thresh==thres].prediction_cross_acc == 2).sum(),
-        (extracted_data[extracted_data.recognition_thresh==thres].prediction_cross_acc == 3).sum(),
-        ], index = cross_total.columns)
-    cross_total = cross_total.append(buf, ignore_index=True)
-
-cross_total_acc_rate = pd.DataFrame(columns=["val", "type", "thres"])
-for type in ["intervention", "prediction"]:
-    for thres in [0.0, 0.5, 0.8]:
-        val = (cross_total[(cross_total.thres==thres) & (cross_total.type==type)].Hit + cross_total[(cross_total.thres==thres) & (cross_total.type==type)].CR) / (cross_total[(cross_total.thres==thres) & (cross_total.type==type)].Hit + cross_total[(cross_total.thres==thres) & (cross_total.type==type)].Miss + cross_total[(cross_total.thres==thres) & (cross_total.type==type)].FA + cross_total[(cross_total.thres==thres) & (cross_total.type==type)].CR)
-        buf = pd.Series([val, type, thres], index=cross_total_acc_rate.columns)
-        cross_total_acc_rate = cross_total_acc_rate.append(buf, ignore_index=True)
-
-cross_length_acc_rate = pd.DataFrame(columns=["val", "type", "thres"])
-for thres in [0.0, 0.5, 0.8]:
-    val = (cross_total[(cross_total.thres==thres) & (cross_total.type==type)].Hit + cross_total[(cross_total.thres==thres) & (cross_total.type==type)].CR) / (cross_total[(cross_total.thres==thres) & (cross_total.type==type)].Hit + cross_total[(cross_total.thres==thres) & (cross_total.type==type)].Miss + cross_total[(cross_total.thres==thres) & (cross_total.type==type)].FA + cross_total[(cross_total.thres==thres) & (cross_total.type==type)].CR)
-    buf = pd.Series([val, type, thres], index=cross_total_acc_rate.columns)
-    cross_total_acc_rate = cross_total_acc_rate.append(buf, ignore_index=True)
-
-acc_length = pd.DataFrame(columns=["type", "count", "length", "thres"])
-for thres in [0.0, 0.5, 0.8]:
-    for length in extracted_data.length.unique():
-        for i, type in enumerate(["Hit", "Miss", "FA", "CR"]):
-            count = (extracted_data[(extracted_data.recognition_thresh==thres) & (extracted_data.length == length)].int_cross_acc == i).sum()
-            buf = pd.Series([type, count, length, thres], index = acc_length.columns)
-            acc_length = acc_length.append(buf, ignore_index=True)
-
-sns.barplot(x="type", y="count", hue="length", data=acc_length[acc_length.thres == 0.0])
-sns.barplot(x="type", y="count", hue="length", data=acc_length[acc_length.thres == 0.5])
-sns.barplot(x="type", y="count", hue="length", data=acc_length[acc_length.thres == 0.8])
-
-
-intv_count_length = pd.DataFrame(columns=["type", "count", "length", "thres"])
-for thres in [0.0, 0.5, 0.8]:
-    for length in extracted_data.length.unique():
-        count = len(extracted_data[(extracted_data.recognition_thresh==thres) & (extracted_data.length == length) & (extracted_data.int_count > 0)])
-        buf = pd.Series(["intervention", count, length, thres], index = intv_count_length.columns)
-        intv_count_length = intv_count_length.append(buf, ignore_index=True)
-
-        count = len(extracted_data[(extracted_data.recognition_thresh==thres) & (extracted_data.length == length) & ((extracted_data.prediction_cross_acc==1) | (extracted_data.prediction_cross_acc==2))])
-        buf = pd.Series(["necessary", count, length, thres], index = intv_count_length.columns)
-        intv_count_length = intv_count_length.append(buf, ignore_index=True)
-
-sns.barplot(x="length", y="count", hue="type", data=intv_count_length[intv_count_length.thres == 0.0])
-sns.barplot(x="length", y="count", hue="type", data=intv_count_length[intv_count_length.thres == 0.5])
-sns.barplot(x="length", y="count", hue="type", data=intv_count_length[intv_count_length.thres == 0.8])
-
-intv_acc_length = pd.DataFrame(columns=["type", "count", "length", "thres"])
-for thres in [0.0, 0.5, 0.8]:
-    for length in extracted_data.length.unique():
-        for i, type in enumerate(["Hit", "Miss", "FA", "CR"]):
-            count = (extracted_data[(extracted_data.recognition_thresh==thres) & (extracted_data.length == length) & (extracted_data.int_count > 0)].int_cross_acc == i).sum()
-            buf = pd.Series([type, count, length, thres], index = intv_acc_length.columns)
-            intv_acc_length = intv_acc_length.append(buf, ignore_index=True)
-
-sns.barplot(x="type", y="count", hue="length", data=intv_acc_length[intv_acc_length.thres == 0.0])
-sns.barplot(x="type", y="count", hue="length", data=intv_acc_length[intv_acc_length.thres == 0.5])
-sns.barplot(x="type", y="count", hue="length", data=intv_acc_length[intv_acc_length.thres == 0.8])
-
-
-intention_count_data = pd.DataFrame(columns=["Hit", "Miss", "FA", "CR", "length", "thres"])
-for thres in [0.0, 0.5, 0.8]:
-    buf = pd.Series([
-        (extracted_data[extracted_data.recognition_thresh==thres].int_acc == 0).sum(),
-        (extracted_data[extracted_data.recognition_thresh==thres].int_acc == 1).sum(),
-        (extracted_data[extracted_data.recognition_thresh==thres].int_acc == 2).sum(),
-        (extracted_data[extracted_data.recognition_thresh==thres].int_acc == 3).sum(),
-        "total",
-        thres,
-        ], index = acc_count_data.columns)
-    intention_count_data = acc_count_data.append(buf, ignore_index=True)
-
-    for length in extracted_data.length.unique():
-        buf = pd.Series([
-            (extracted_data[(extracted_data.recognition_thresh==thres) & (extracted_data.length == length)].int_acc == 0).sum(),
-            (extracted_data[(extracted_data.recognition_thresh==thres) & (extracted_data.length == length)].int_acc == 1).sum(),
-            (extracted_data[(extracted_data.recognition_thresh==thres) & (extracted_data.length == length)].int_acc == 2).sum(),
-            (extracted_data[(extracted_data.recognition_thresh==thres) & (extracted_data.length == length)].int_acc == 3).sum(),
-            length,
-            thres,
-            ], index = acc_count_data.columns)
-        intention_count_data = acc_count_data.append(buf, ignore_index=True)
-
-int_length_data = pd.DataFrame(columns=[1.0, 3.0, 5.0, 7.0, 9.0, 12.0])
-for length in int_length_data.columns:
-    index = float(length)
-    target = extracted_data[extracted_data.length==index]
-    int_length_data.loc["intervention_total", index] = len(target[((target.result_acc == 0)|(target.result_acc == 3))]) / len(target)
-    int_length_data.loc["prediction", index] = len(target[((target.prediction_prob > 0.5) & (target.annt_prob > 0.5)) | ((target.prediction_prob <= 0.5) & (target.annt_prob <= 0.5))]) / len(target)
-    int_length_data.loc["total", index] = len(target[((target.last_intention==True) & (target.annt_prob<=0.5)) | ((target.last_intention==False) & (target.annt_prob>0.5))]) / len(target)
+extracted_data.to_csv(data_path+"/summary2.csv")
