ff59303c82594bc9ce9c63248e0e757d8f048fea
kuriatsu <kuriatsubayashi712@gmail.com>
2022-05-24 14:48:46 +0900

    add tlr evaluation

--- tl/train_tlr.py
+++ tl/train_tlr.py
@@ -8,6 +8,7 @@ from keras.layers.normalization import BatchNormalization
 from keras.callbacks import ModelCheckpoint
 import numpy as np
 import pickle
+import pandas as pd
 
 def model():
     tlcat_model = Sequential()
@@ -38,12 +39,20 @@ def train(x_train, y_train, x_valid, y_valid):
     checkpointer = ModelCheckpoint(filepath="model.weights.traffic_lights.hdf5", verbose=1, save_best_only=True)
     tlcat_model.fit(x_train, y_train, batch_size=64, epochs=20, validation_data=(x_valid, y_valid), callbacks=[checkpointer], verbose=2, shuffle=True)
 
-def test(x_test, y_test):
+def test(x_test, y_test, x_test_id):
     tlcat_model = model()
     tlcat_model.load_weights("model.weights.traffic_lights.hdf5")
-    predictions = [np.argmax(tlcat_model.predict(np.expand_dims(feature, axis=0))) for feature in x_test]
-    test_accuracy = 100*np.sum(np.array(predictions)==np.argmax(y_test, axis=1))/len(predictions)
-    print('Test accuracy: %.4f%%' % test_accuracy)
+    # predictions = [np.argmax(tlcat_model.predict(np.expand_dims(feature, axis=0))) for feature in x_test]
+    df = pd.DataFrame(columns=["id", "result", "gt"])
+    for i in range(0, len(x_test)):
+        result = np.argmax(tlcat_model.predict(np.expand_dims(x_test[i], axis=0)))
+        gt = np.argmax(y_test[i])
+        buf = pd.DataFrame([(x_test_id[i], result, gt)], columns=df.columns)
+        df = pd.concat([df, buf])
+    # test_accuracy = 100*np.sum(np.array(predictions)==np.argmax(y_test, axis=1))/len(predictions)
+    print(df)
+    df.to_csv("tlr_result.csv")
+    # print('Test accuracy: %.4f%%' % test_accuracy)
 
 
 def main():
@@ -52,14 +61,15 @@ def main():
     x_valid = []
     y_valid = []
     x_test = []
+    x_test_id = []
     y_test = []
 
     image_set_nums = {
         'train': ['set01', 'set02', 'set04'],
         'val'  : ['set05', 'set06'],
-        'test' : ['set03'],
+        'test' : ['test'],
         'all'  : ['set01', 'set02', 'set03',
-                  'set04', 'set05', 'set06']
+                  'set04', 'set05', 'set06'],
         }
     with open("/home/kuriatsu/share/database.pickle", "rb") as f:
         database = pickle.load(f)
@@ -73,11 +83,12 @@ def main():
             y_valid.append(data.get("state"))
         elif data.get("set") in image_set_nums.get("test"):
             x_test.append(data.get("image"))
+            x_test_id.append(data.get("id"))
             y_test.append(data.get("state"))
 
     print("len", len(np.array(y_test)), len(x_test[0]), len(x_test[0][0]))
     # train(np.array(x_train), np.array(y_train), np.array(x_valid), np.array(y_valid))
-    test(np.array(x_test), np.array(y_test))
+    test(x_test, y_test, x_test_id)
 
 if __name__ == "__main__":
     main()
